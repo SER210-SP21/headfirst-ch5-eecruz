40393410104d6ee2d019e888a8b966f1bc09bb24
Emilio Cruz <eecruz@quinnpiac.edu>
2023-03-03 19:18:03 -0500

    chapter 5 demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/stopwatch/MainActivity.kt
@@ -0,0 +1,124 @@
+package edu.quinnipiac.ser210.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+import java.time.temporal.ChronoField
+
+class MainActivity : AppCompatActivity()
+{
+    lateinit var stopwatch: Chronometer
+    var running = false
+    var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener()
+        {
+            if (!running)
+            {
+                setBaseTime()
+                stopwatch.start()
+                running = true
+            }
+        }
+
+        val pauseButton = findViewById<Button>(R.id.pause_button)
+        pauseButton.setOnClickListener()
+        {
+            if (running)
+            {
+                saveOffset()
+                stopwatch.stop()
+                running = false
+            }
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener()
+        {
+            offset = 0
+            setBaseTime()
+        }
+
+        if(savedInstanceState != null)
+        {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if(running)
+            {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            }
+            else
+                setBaseTime()
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle)
+    {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle)
+    {
+        offset = savedInstanceState.getLong(OFFSET_KEY)
+        running = savedInstanceState.getBoolean(RUNNING_KEY)
+        if (running)
+        {
+            stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+            stopwatch.start()
+        } else
+            setBaseTime()
+
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        if(running)
+        {
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+
+    override fun onResume()
+    {
+        super.onResume()
+        if(running)
+        {
+            setBaseTime()
+            stopwatch.start()
+            offset = 0
+        }
+    }
+
+    fun setBaseTime()
+    {
+        stopwatch.base = SystemClock.elapsedRealtime()
+    }
+
+    fun saveOffset()
+    {
+        offset = SystemClock.elapsedRealtime() - stopwatch.base
+    }
+}
